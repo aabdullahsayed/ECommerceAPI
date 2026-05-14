0922511653687b51114311c2283f408294b5d5a5
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-05-14 20:50:11 +0600

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using ECommerceAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString)
+    );
+
+var app = builder.Build();
+
+app.Run();
+
+
